e4c99d393cb1fc35c267beaddeff898e5d505fb8
O'Shaughnessy, Peter <Peter.OShaughnessy@pearson.com>
2016-09-01 15:26:56 +0100

    Add session cookie

--- js/main.js
+++ js/main.js
@@ -1,19 +1,33 @@
-var COOKIE_NAME = 'myTestCookie';
-var GOT_COOKIE_MSG = 'You got a cookie';
-var NO_COOKIE_MSG = 'No cookie yet, let me set one...';
+var PERSISTENT_COOKIE_NAME = 'testPersistentCookie';
+var SESSION_COOKIE_NAME = 'testSessionCookie';
+
+var GOT_PERSISTENT_COOKIE_MSG = 'Persistent cookie: Got. ';
+var GOT_SESSION_COOKIE_MSG = 'Session cookie: Got. ';
+var NO_COOKIE_MSG = 'No cookies yet, let me set them...';
+
 var COOKIE_MAX_AGE = 15552000; // 6 months in seconds
 
-var myCookie = docCookies.getItem(COOKIE_NAME);
+var persistentCookie = docCookies.getItem(PERSISTENT_COOKIE_NAME);
+var sessionCookie = docCookies.getItem(SESSION_COOKIE_NAME);
 
-if (myCookie) {
+var message = '';
 
-  DemoScene.setMessage(GOT_COOKIE_MSG + ': ' + myCookie);
+if (persistentCookie) {
+  message += GOT_PERSISTENT_COOKIE_MSG;
+}
 
-} else {
+if (sessionCookie) {
+  message += GOT_SESSION_COOKIE_MSG;
+}
 
-  DemoScene.setMessage(NO_COOKIE_MSG);
+if (!message.length) {
 
-  docCookies.setItem(COOKIE_NAME, 'Hello WebVR!', COOKIE_MAX_AGE);
+  message = NO_COOKIE_MSG;
+
+  docCookies.setItem(PERSISTENT_COOKIE_NAME, 'WebVR rocks!', COOKIE_MAX_AGE);
+  docCookies.setItem(SESSION_COOKIE_NAME, 'WebVR rocks for this session!');
 
 }
+
+DemoScene.setMessage(message);
 
